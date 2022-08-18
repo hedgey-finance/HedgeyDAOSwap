bfa5bda7c4e08d93d1630c54cef93c533d10efc2
Mark Howard <bark4mark@gmail.com>
2022-08-18 17:08:12 +0100

    feat: Adding config for sol coverage

---
+++ .solcover.js
@@ -0,0 +1,11 @@
+module.exports = {
+  skipFiles: [
+    'interfaces/INFT.sol',
+    'interfaces/IWeth.sol',
+    'libraries/NFTHelper.sol',
+    'libraries/TransferHelper.sol',
+    'test/FuturesNFT.sol',
+    'test/Token.sol',
+    'test/WETH9.sol',
+  ],
+};
