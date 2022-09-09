fe37575be685e6ca0e00c41d6c9bea5b23d45a21
Alex Michelsen <57596674+icemanparachute@users.noreply.github.com>
2022-09-09 19:21:25 -0400

    remove hardhat console

--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.13;
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '../interfaces/IWeth.sol';
-import 'hardhat/console.sol';
 
 /// @notice Library to help safely transfer tokens and handle ETH wrapping and unwrapping of WETH
 library TransferHelper {
